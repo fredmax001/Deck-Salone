53e69bf8f4035fad5b156d34f5a980e9489ccf6c
fredmax002 <djfredmax221@gmail.com>
2026-07-14 17:52:10 +0800

    changes

--- src-remote/pages/Discover.tsx
+++ src-remote/pages/Discover.tsx
@@ -717,7 +717,7 @@ export default function Discover() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+                className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6"
               >
                 <AnimatePresence>
                   {displayedDjs.map((dj, i) => (
